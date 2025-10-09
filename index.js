363ec5b85dbd21e636c22d5812962edf73c84513
unknown <vshrivastav933@gmail.com>
2025-10-09 20:18:41 +0530

    chore: connected deployed backend with frontend instead of localhost

--- index.js
+++ index.js
@@ -19,11 +19,12 @@ const rejectedRouter = require("./routes/RejectedProviderRoute");
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://day-to-day-frontend.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
     credentials: true,
   })
 );
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
