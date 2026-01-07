361a77c8b06c81da196805cff4b3bf5a5dd30640
unknown <vshrivastav933@gmail.com>
2026-01-07 12:55:57 +0530

    remove cluster

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const cluster = require("cluster");
-const os = require("os");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -8,17 +6,11 @@ const connectDB = require("./config/db");
 const app = require("./app");
 
 const PORT = process.env.PORT;
-const numCPUs = os.cpus().length;
 
-if (cluster.isMaster) {
-  console.log(`Master PID: ${process.pid}`);
-  for (let i = 0; i < numCPUs; i++) cluster.fork();
-
-  cluster.on("exit", () => cluster.fork());
-} else {
-  connectDB();
+(async () => {
+  await connectDB();
 
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT} | Worker PID: ${process.pid}`);
+    console.log(`Server running on port ${PORT}`);
   });
-}
+})();
