e73064cebade054c9d55655ef37ff6a0353e84c8
unknown <vshrivastav933@gmail.com>
2026-01-08 17:05:42 +0530

    fix cors error

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
