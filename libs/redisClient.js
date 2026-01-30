cb5472a8d66634c6c146e0961ee7058f615acc46
unknown <vshrivastav933@gmail.com>
2026-01-30 14:08:52 +0530

    set redis url

--- libs/redisClient.js
+++ libs/redisClient.js
@@ -1,6 +1,8 @@
 const redis = require("redis");
 
-const client = redis.createClient();
+const client = redis.createClient({
+  url: process.env.REDIS_URL,
+});
 
 client.on("connect", () => {
   console.log("Redis connected");
